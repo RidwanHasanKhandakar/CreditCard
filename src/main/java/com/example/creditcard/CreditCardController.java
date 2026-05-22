ece6cf310b71152367c5480b9a8fe12412d91a0a
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-22 19:30:56 +0600

    updated the controller class

--- src/main/java/com/example/creditcard/CreditCardController.java
+++ src/main/java/com/example/creditcard/CreditCardController.java
@@ -1,7 +1,50 @@
 package com.example.creditcard;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class CreditCardController
 {
     @javafx.fxml.FXML
+    private TextField cardNoTextField;
+    @javafx.fxml.FXML
+    private TableColumn holderNameCol;
+    @javafx.fxml.FXML
+    private TableColumn gatewayNameCol;
+    @javafx.fxml.FXML
+    private TextField creditLimitTextField;
+    @javafx.fxml.FXML
+    private DatePicker dateOfExpiryDatePicker;
+    @javafx.fxml.FXML
+    private TableView creditCardTableView;
+    @javafx.fxml.FXML
+    private TextField holderNameTextField;
+    @javafx.fxml.FXML
+    private TableColumn creditLimitCol;
+    @javafx.fxml.FXML
+    private TextField searchCreditLimitTextField;
+    @javafx.fxml.FXML
+    private ComboBox cardTypeComboBox;
+    @javafx.fxml.FXML
+    private ComboBox gatewayNameComboBox;
+    @javafx.fxml.FXML
+    private ComboBox searchGatewayNameComboBox;
+    @javafx.fxml.FXML
+    private TableColumn cardNoCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSearchButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleShowAvrgButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleValidateButton(ActionEvent actionEvent) {
+    }
+}
